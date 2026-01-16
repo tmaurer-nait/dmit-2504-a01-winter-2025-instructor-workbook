06569a625fe220ddec1edf1b55a8ec05d25e09e7
Tom Maurer <tmaurer@nait.ca>
2026-01-16 10:49:54 -0700

    async pt 1

---
+++ Dart_intro/async_ghibli/lib/services/ghibli_api_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const endpoint = 'https://ghibliapi.vercel.app/films/';
+
+// Fetches a ghibli film given an id, and returns a decoded json representing that film
+Future<dynamic> getFilmById(String id) async {
+  // TODO: Parse the endpoint URI
+
+  // TODO: Fetch the data from the endpoint
+
+  // TODO: decode the response
+
+  // TODO: return the decoded json
+
+  // TODO: Error handling for status codes
+
+  // TODO: Error handling for any other issues
+}
