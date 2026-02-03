e81fddd342644d4e39b642f1abebb5c9fbcdc02c
Tom Maurer <tmaurer@nait.ca>
2026-02-03 13:47:20 -0700

    forms pt2

--- Widgets/forms_example/lib/main.dart
+++ Widgets/forms_example/lib/main.dart
@@ -13,39 +13,48 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'New User Sign Up Form',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              TextField(decoration: InputDecoration(label: Text('Username'))),
-              TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(label: Text('Password')),
-              ),
-              TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  label: Text('Confirm Your Password'),
-                ),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  foregroundColor: Colors.white,
-                ),
-                onPressed: () {},
-                child: Text('Sign Up'),
-              ),
-            ],
-          ),
+          child: UserSignupForm(),
         ),
+      ),
+    );
+  }
+}
+
+class UserSignupForm extends StatelessWidget {
+  const UserSignupForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'New User Sign Up Form',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          TextFormField(decoration: InputDecoration(label: Text('Username'))),
+          TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(label: Text('Password')),
+          ),
+          TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(label: Text('Confirm Your Password')),
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            child: Text('Sign Up'),
+          ),
+        ],
       ),
     );
   }
