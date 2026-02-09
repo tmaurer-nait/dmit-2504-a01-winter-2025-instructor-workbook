c7f494ae4963293a1d0399c9b5ece498d688e914
Tom Maurer <tmaurer@nait.ca>
2026-02-09 08:22:57 -0700

    nested nav pt 2

--- navigation/nested_nav/lib/main.dart
+++ navigation/nested_nav/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nested_nav/pages/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      // OnGenerateRoute allows us to return different pages based on
+      // some function that we determine. It is passed in the Route Settings
+      // of the current page request.
+      onGenerateRoute: (settings) {
+        // This will hold the value of whatever page we are routing to
+        Widget page;
+
+        switch (settings.name) {
+          case '/':
+            page = HomePage();
+            break;
+          default:
+            // This is where you can handle routing to a non-existant route
+            // eg. 404 not found page
+            throw Exception('Unknown Route: ${settings.name}');
+        }
+        return MaterialPageRoute(
+          builder: (context) => page,
+          settings: settings,
+        );
+      },
     );
   }
 }
