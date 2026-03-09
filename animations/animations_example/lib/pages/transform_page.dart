53a278a6b56eece8b2ce91540a94babece8b6252
Tom Maurer <tmaurer@nait.ca>
2026-03-09 08:47:35 -0600

    transforms pt 2

--- animations/animations_example/lib/pages/transform_page.dart
+++ animations/animations_example/lib/pages/transform_page.dart
@@ -1,4 +1,7 @@
+import 'package:animations_example/pages/animation_page.dart';
 import 'package:flutter/material.dart';
+// Needed for pi
+import 'dart:math' as math;
 
 class TransformPage extends StatelessWidget {
   const TransformPage({super.key});
@@ -10,8 +13,46 @@ class TransformPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            // Wrap a widget in a transform to change it
+            Transform.rotate(
+              angle: 45 * math.pi / 180, // Angle is measured in radians
+              child: Text('Rotate this text', style: TextStyle(fontSize: 30)),
+            ),
+            SizedBox(height: 100),
+            Transform.scale(
+              scale: 2,
+              child: Text('Scale this text', style: TextStyle(fontSize: 30)),
+            ),
+            SizedBox(height: 100),
+            Transform.translate(
+              // x and y offset (positive is right,down)
+              offset: Offset(50, -500),
+              child: Text(
+                'Translate this text',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            Transform.rotate(
+              angle: math.pi,
+              child: Transform.scale(
+                scaleX: 3,
+                child: Transform.translate(
+                  offset: Offset(30, -40),
+                  child: Text('Rotate, scale, translate'),
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => AnimationPage()));
+        },
       ),
     );
   }
