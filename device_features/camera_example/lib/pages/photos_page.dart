8c8716e38ff092cce8a221694cc225d207acd48b
Tom Maurer <tmaurer@nait.ca>
2026-03-10 14:44:32 -0600

    cameras done

--- device_features/camera_example/lib/pages/photos_page.dart
+++ device_features/camera_example/lib/pages/photos_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'dart:io';
+
+// flutter pub add path_provider
+import 'package:path_provider/path_provider.dart';
+
 class PhotosPage extends StatefulWidget {
   const PhotosPage({super.key});
 
@@ -8,8 +13,38 @@ class PhotosPage extends StatefulWidget {
 }
 
 class _PhotosPageState extends State<PhotosPage> {
+  var photosDir = '';
+  List<String> photos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // get the cache folder where I saved my photos to
+    getApplicationCacheDirectory().then((dir) {
+      setState(() {
+        // Save the cache location
+        photosDir = dir.path;
+
+        // for each file in cache, if it's a jpg save that file path to photos
+        dir.listSync().toList().forEach((file) {
+          if (file.path.endsWith('jpg')) photos.add(file.path);
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, idx) {
+          return ListTile(
+            leading: Image.file(File(photos[idx]), height: 50, width: 50),
+            title: Text(photos[idx]),
+          );
+        },
+        itemCount: photos.length,
+      ),
+    );
   }
 }
