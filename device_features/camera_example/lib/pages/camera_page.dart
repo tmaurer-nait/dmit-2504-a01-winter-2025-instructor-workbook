3408032ac247da9deb1f92205f80a12f70ab5d7d
Tom Maurer <tmaurer@nait.ca>
2026-03-10 14:30:47 -0600

    taking pictures'

--- device_features/camera_example/lib/pages/camera_page.dart
+++ device_features/camera_example/lib/pages/camera_page.dart
@@ -42,6 +42,29 @@ class _CameraPageState extends State<CameraPage> {
         });
   }
 
+  Future<XFile?> _takePicture() async {
+    // first make sure we're not currently taking a photo
+    if (_camController.value.isTakingPicture) return null;
+
+    try {
+      var file = await _camController.takePicture();
+      // When I take the picture the file automatically gets saved into a local cache
+      // We can move it to more permanent storage
+      // if we like or just read it from there later on
+      return file;
+    } catch (e) {
+      // If something goes wrong while taking a picture handle that here
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    // don't forget to dispose controllers to prevent memory leaks
+    _camController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +73,16 @@ class _CameraPageState extends State<CameraPage> {
       // Camera preview
       body: Center(child: Expanded(child: CameraPreview(_camController))),
       // Take a picture button
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var file = await _takePicture();
+          if (file != null && mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Picture saved to ${file.path}')),
+            );
+          }
+        },
+      ),
     );
   }
 }
